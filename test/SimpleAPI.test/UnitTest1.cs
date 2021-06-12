5e4ac747e2d51610a8e2f423d4635596cf089d7d
Sandip Practice <sandipghoshn67@hotmail.com>
2021-06-12 19:29:03 +0200

    Initial Commit

---
+++ test/SimpleAPI.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
